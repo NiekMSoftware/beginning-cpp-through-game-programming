21a9d4ba24d11aafb03072fd3808339e9b6679e7
NiekMSoftware <niek+github@meletstudios.com>
2025-09-07 13:16:56 +0200

    lost fortune

--- chapter1/main.cpp
+++ chapter1/main.cpp
@@ -1,6 +1,7 @@
 // Chapter 1 with all the examples
 
 #include <iostream>
+#include <string>
 
 static void game_over() {
 	std::cout << "Game Over!" << std::endl;
@@ -103,11 +104,57 @@ static void game_stats_03() {
 		<< (CRUISER_COST - myShipCost) << " Resource Points.\n";
 }
 
+static void lost_fortune() {
+	using std::cout;
+	using std::cin;
+	using std::endl;
+	using std::string;
+
+	const int GOLD_PIECES = 900;
+	int adventurers, killed, survivors;
+	string leader;
+
+	// get information
+	cout << "Welcome to Lost Fortune\n\n";
+	cout << "Please enter the following for your personalized adventure\n";
+
+	cout << "Enter a number: ";
+	cin >> adventurers;
+
+	cout << "Enter a number, smaller than the first: ";
+	cin >> killed;
+
+	survivors = adventurers - killed;
+
+	cout << "Enter your last name: ";
+	cin >> leader;
+
+	// tell the story
+	cout << "\nA brave group of " << adventurers << " set out on a quest ";
+	cout << "-- in search of the lost treasure of the Ancient Dwarves. ";
+	cout << "The group was led by that legendary rogue, " << leader << ".\n";
+
+	cout << "\nAlong the way, a band of marauding ogres ambushed the party. ";
+	cout << "All fought bravely under the command of " << leader;
+	cout << ", and the ogres were defeated, but at a cost. ";
+	cout << "Of the adventurers, " << killed << " were vanquished, ";
+	cout << "leaving just " << survivors << " in the group.\n";
+
+	cout << "\nThe party was about to give up all hope. ";
+	cout << "But while laying the deceased to rest, ";
+	cout << "they stumbled upon the buried fortune. ";
+	cout << "So the adventurers split " << GOLD_PIECES << " gold pieces.";
+	cout << leader << " held on to the extra " << (GOLD_PIECES % survivors);
+	cout << " pieces to keep things fair of course.\n";
+}
+
 int main() {
 	//game_over();
 	//expensive_calculator();
 	//game_stats_01();
 	//game_stats_02();
-	game_stats_03();
+	//game_stats_03();
+	lost_fortune();
+
 	return 0;
 }
