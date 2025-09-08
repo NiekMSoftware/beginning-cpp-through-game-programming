6e49d609488ca27c4bccacbc759c15937aefb4e8
NiekMSoftware <niek+github@meletstudios.com>
2025-09-08 14:45:58 +0200

    sequence of if statements

--- chapter_02/main.cpp
+++ chapter_02/main.cpp
@@ -41,8 +41,25 @@ void score_rater_02() {
 	}
 }
 
+void score_rater_03() {
+	int score;
+	cout << "Enter your score: ";
+	cin >> score;
+
+	if (score >= 1000) {
+		cout << "You scored 1000 or more. Impressive!\n";
+	} else if (score >= 500) {
+		cout << "You scored 500 or more. Nice!\n";
+	} else if (score >= 250) {
+		cout << "You scored 250 or more. Decent!\n";
+	} else {
+		cout << "You scored less than 250. Nothing to brag about.\n";
+	}
+}
+
 int main() {
 	//using_if_statement();
 	//score_rater_02();
+	//score_rater_03();
 	return 0;
 }
